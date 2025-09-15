1feff6ac62607462c3da325766e4c14ff5667901
Tres Seaver <tseaver@palladion.com>
2025-09-15 15:56:38 -0400

    feat: FastAPI app setup
    Include 'example' installation directory, and use it if no
    installation path is passed direcctly or via the environment.

---
+++ src/soliplex/main.py
@@ -0,0 +1,72 @@
+import contextlib
+import functools
+import os
+import pathlib
+import sys
+
+import fastapi
+import uvicorn
+from fastapi.middleware import cors as fastapi_mw_cors
+from starlette.middleware import sessions as starlette_mw_sessions
+
+from soliplex import installation
+from soliplex import rooms
+from soliplex import util
+
+
+def curry_lifespan(installation_path: pathlib.Path=None):
+    if installation_path is None:
+        installation_path = os.environ.get("INSTALLATION_PATH")
+
+        if installation_path is None:
+            installation_path = "./example"
+
+    installation_path = pathlib.Path(installation_path)
+
+    return functools.partial(
+        installation.lifespan, installation_path=installation_path,
+    )
+
+def create_app(installation_path: pathlib.Path=None):  # pragma: NO COVER
+    curried_lifespan = curry_lifespan(installation_path)
+    acm_lifespan = contextlib.asynccontextmanager(curried_lifespan)
+    app = fastapi.FastAPI(lifespan=acm_lifespan)
+
+    origins = ["*"]
+    app.add_middleware(
+        fastapi_mw_cors.CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.add_middleware(
+        starlette_mw_sessions.SessionMiddleware,
+        # Deliberately not an envvar
+        secret_key=os.urandom(16).hex(),
+    )
+
+    current_git_hash = util.get_git_hash_for_file(__file__)
+
+    @app.middleware("http")
+    async def add_custom_header(request: fastapi.Request, call_next):
+        response: fastapi.Response = await call_next(request)
+        response.headers["X-Git-Hash"] = current_git_hash
+        return response
+
+    app.include_router(installation.router, prefix="/api")
+    app.include_router(rooms.router, prefix="/api")
+
+    return app
+
+
+if __name__ == "__main__":  # pragma:  NO COVER
+    if sys.argv:
+        installation_path = sys.argv[1]
+    else:
+        installation_path = None
+
+    app = create_app(pathlib.Path(installation_path))
+
+    uvicorn.run(app, port=8000)
---
+++ tests/unit/test_main.py
@@ -0,0 +1,43 @@
+import functools
+import pathlib
+from unittest import mock
+
+import pytest
+
+from soliplex import main
+
+EXPLICIT_INST_PATH = "/explicit"
+ENVIRON_INST_PATH = "/environ"
+
+
+@pytest.fixture(scope="module", params=[None, EXPLICIT_INST_PATH])
+def inst_path_kwargs(request):
+    kw = {}
+    if request.param is not None:
+        kw["installation_path"] = request.param
+    return kw
+
+
+@pytest.mark.parametrize("env_patch", [
+    {},
+    {"INSTALLATION_PATH": ENVIRON_INST_PATH},
+])
+def test_curry_lifespan(inst_path_kwargs, env_patch):
+
+    with mock.patch.dict("os.environ", clear=True, **env_patch):
+        found = main.curry_lifespan(**inst_path_kwargs)
+
+    assert isinstance(found, functools.partial)
+
+    if inst_path_kwargs:
+        expected = EXPLICIT_INST_PATH
+
+    elif env_patch:
+        expected = ENVIRON_INST_PATH
+
+    else:
+        expected = "./example"
+
+    assert found.keywords == {
+        "installation_path": pathlib.Path(expected),
+    }
