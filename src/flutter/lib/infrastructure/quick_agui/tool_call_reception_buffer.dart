84f326927fca8c1dac1ef069894a90e4dc2b209f
Jaemin Jo <44039707+91jaeminjo@users.noreply.github.com>
2025-11-26 14:27:20 -0600

    toolCallReceptionBuffer to generate assistant message

--- src/flutter/lib/infrastructure/quick_agui/tool_call_reception_buffer.dart
+++ src/flutter/lib/infrastructure/quick_agui/tool_call_reception_buffer.dart
@@ -1,12 +1,29 @@
+import 'package:ag_ui/ag_ui.dart' as ag_ui;
+
 class ToolCallReceptionBuffer {
+  final String id;
   final String name;
   final StringBuffer _argsBuffer;
 
-  ToolCallReceptionBuffer(this.name) : _argsBuffer = StringBuffer();
+  ToolCallReceptionBuffer(this.id, this.name) : _argsBuffer = StringBuffer();
 
   void appendArgs(String delta) {
     _argsBuffer.write(delta);
   }
 
   String get args => _argsBuffer.toString();
+
+  ag_ui.AssistantMessage get toMessage => ag_ui.AssistantMessage(
+    // TODO: may need to get msg some other way (generate it or retrieve it from server).
+    id: 'msg_$id',
+    toolCalls: [
+      ag_ui.ToolCall(
+        id: id,
+        function: ag_ui.FunctionCall(
+          name: name,
+          arguments: args.isEmpty ? '{}' : args,
+        ),
+      ),
+    ],
+  );
 }
