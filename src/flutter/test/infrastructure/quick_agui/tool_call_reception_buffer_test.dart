798ac9a6a307f521376bdacefeb8e4a801dfe388
Jaemin Jo <44039707+91jaeminjo@users.noreply.github.com>
2025-11-26 15:51:05 -0600

    add toolCall getter for ToolCallReceptionBuffer

--- src/flutter/test/infrastructure/quick_agui/tool_call_reception_buffer_test.dart
+++ src/flutter/test/infrastructure/quick_agui/tool_call_reception_buffer_test.dart
@@ -7,44 +7,65 @@ import 'package:soliplex_client/infrastructure/quick_agui/tool_call_reception_bu
 void main() {
   group('ToolCallReceptionBuffer class', () {
     test('should accept an id and a name parameter and expose them', () {
-      // Arrange & Act
       final buffer = ToolCallReceptionBuffer('tool-call-id', 'tool-call-name');
 
-      // Assert
-      expect(buffer.name, equals('tool-call-id'));
+      expect(buffer.id, equals('tool-call-id'));
       expect(buffer.name, equals('tool-call-name'));
     });
 
     test('args initially empty, can be appended', () {
-      // Arrange
       final buffer = ToolCallReceptionBuffer('tool-call-id', 'add-numbers');
       expect(buffer.args, equals(''));
 
-      // Act
       buffer.appendArgs("{'arg1':");
       buffer.appendArgs(" 1, '");
       buffer.appendArgs("arg2'");
       buffer.appendArgs(": 2}");
 
-      // Assert
       expect(buffer.args, equals("{'arg1': 1, 'arg2': 2}"));
     });
 
-    test('generate an assistant message with proper values', () {
-      // Arrange
+    test('generate a tool call with proper values', () {
       const toolCallId = 'tool-call-id';
       const toolCallName = 'add-numbers';
       final buffer = ToolCallReceptionBuffer(toolCallId, toolCallName);
       expect(buffer.args, equals(''));
 
-      // Act
       buffer.appendArgs("{'arg1':");
       buffer.appendArgs(" 1, '");
       buffer.appendArgs("arg2'");
       buffer.appendArgs(": 2}");
 
-      // Assert
-      final message = buffer.toMessage;
+      final toolCall = buffer.toolCall;
+      expect(
+        toolCall,
+        isA<ag_ui.ToolCall>().having(
+          (m) => m.toJson(),
+          'entire toolCall in json',
+          equals(toolCall.toJson()),
+        ),
+      );
+
+      expect(toolCall.id, equals(toolCallId));
+
+      final functionCall = toolCall.function;
+
+      expect(functionCall.name, toolCallName);
+      expect(functionCall.arguments, "{'arg1': 1, 'arg2': 2}");
+    });
+
+    test('generate an assistant message with proper values', () {
+      const toolCallId = 'tool-call-id';
+      const toolCallName = 'add-numbers';
+      final buffer = ToolCallReceptionBuffer(toolCallId, toolCallName);
+      expect(buffer.args, equals(''));
+
+      buffer.appendArgs("{'arg1':");
+      buffer.appendArgs(" 1, '");
+      buffer.appendArgs("arg2'");
+      buffer.appendArgs(": 2}");
+
+      final message = buffer.message;
       expect(
         message,
         isA<ag_ui.AssistantMessage>().having(
