df4bcc55640d9f820bf78c43952ceeb8092f5c09
Tres Seaver <tseaver@palladion.com>
2025-12-15 11:16:52 -0500

    chore: erase existing front-end prototypes

--- src/agui_dash_2/android/app/src/main/kotlin/com/example/agui_chat_rfw/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.agui_chat_rfw
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- src/flutter/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'configure.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final app = await configure();
-  runApp(app);
-}
