81a1ad22dad3e8cce1136e553215677d3a8c5f0e
mcdonc <mcdonc@users.noreply.github.com>
2025-12-17 17:01:33 +0000

    deploy: 554c7cfba53bc13d75f51909c930ba4e0b7cde25

--- webapp/flutter_bootstrap.js
+++ webapp/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"b5cdf57426107e7a8eebf469a017833e013894
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1279379378" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1374272989" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
