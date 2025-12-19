c5c7d0f9c1c8d403cde48796a6a584a637860cfc
tseaver <tseaver@users.noreply.github.com>
2025-12-19 15:30:02 +0000

    deploy: f154f9ef1c5d8afb050cfa137ff9882f3319161b

--- webapp/flutter_bootstrap.js
+++ webapp/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"9828a8bf3fe26ba43a8aecf7e8eb1033ac2858
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3355235433" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "687847522" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
