94d72a75a6d51c3f70e35ea2dc78c044026eb671
Tres Seaver <tseaver@palladion.com>
2026-01-17 15:05:08 -0500

    feat: 'ollama.REST_API'
    Query / manipulate Ollama server via its REST API.

--- src/soliplex/ollama.py
+++ src/soliplex/ollama.py
@@ -2,11 +2,136 @@
 Pull Ollama models via HTTP API.
 """
 
+import dataclasses
 import json
+import typing
 import urllib.error
 import urllib.request
 
+import requests
+
 DEFAULT_OLLAMA_BASE_URL = "http://localhost:11434"
+
+
+@dataclasses.dataclass
+class REST_API:
+    ollama_base_url: str = DEFAULT_OLLAMA_BASE_URL
+
+    def _api_url(self, endpoint_name) -> str:
+        return f"{self.ollama_base_url}/api/{endpoint_name}"
+
+    def _get_endpoint(self, endpoint_name):
+        url = self._api_url(endpoint_name)
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        return response.json()
+
+    def _post_endpoint(self, endpoint_name, data):
+        url = self._api_url(endpoint_name)
+
+        response = requests.post(url, json=data)
+        response.raise_for_status()
+
+        return response.json()
+
+    def _delete_endpoint(self, endpoint_name, data):
+        url = self._api_url(endpoint_name)
+
+        response = requests.delete(url, json=data)
+        response.raise_for_status()
+
+        return response.json()
+
+    def get_version(self):
+        """Return the version of the Ollama server"""
+        return self._get_endpoint("version")
+
+    def get_available_models(self):
+        """Return metadata about models available on the Ollama server"""
+        return self._get_endpoint("tags")
+
+    def get_running_models(self):
+        """Return metadata about models running on the Ollama server"""
+        return self._get_endpoint("ps")
+
+    def show_model(self, model_name: str, verbose: bool = None):
+        """Return metadata about an individual model on the Ollama server"""
+        data = {"model": model_name}
+
+        if verbose is not None:
+            data["verbose"] = verbose
+
+        return self._post_endpoint("show", data)
+
+    def create_model(
+        self,
+        *,
+        model_name: str,
+        source_model_name: str,
+        stream: bool = None,
+        template: str = None,
+        license: str | list[str] = None,
+        system_prompt: str = None,
+        parameters: typing.Sequence[dict[str, typing.Any]] = None,
+        messages: str = None,
+        quantize: str = None,
+    ):
+        """Copy a model to a new name / tag on the Ollama server"""
+        data = {
+            "model": model_name,
+            "from": source_model_name,
+        }
+        if stream is not None:
+            data["stream"] = stream
+
+        if template is not None:
+            data["template"] = template
+
+        if license is not None:
+            data["license"] = license
+
+        if system_prompt is not None:
+            data["system"] = system_prompt
+
+        if parameters is not None:
+            data["parameters"] = parameters
+
+        if messages is not None:
+            data["messages"] = messages
+
+        if quantize is not None:
+            data["quantize"] = quantize
+
+        return self._post_endpoint("create", data)
+
+    def copy_model(self, source_model_name: str, dest_model_name: str):
+        """Copy a model to a new name / tag on the Ollama server"""
+        data = {"source": source_model_name, "destination": dest_model_name}
+
+        return self._post_endpoint("copy", data)
+
+    def push_model(self, model_name: str):
+        """Publish a model on the Ollama server"""
+        data = {"model": model_name}
+
+        return self._post_endpoint("push", data)
+
+    def pull_model(self, model_name: str, stream=None):
+        """Download a published model on the Ollama server"""
+        data = {"model": model_name}
+
+        if stream is not None:
+            data["stream"] = stream
+
+        return self._post_endpoint("pull", data)
+
+    def delete_model(self, model_name: str):
+        """Delte a published model on the Ollama server"""
+        data = {"model": model_name}
+
+        return self._delete_endpoint("delete", data)
 
 
 def pull_model(model_name, ollama_url, on_status=None):
--- tests/unit/test_ollama.py
+++ tests/unit/test_ollama.py
@@ -1,8 +1,351 @@
+import contextlib
 import json
 import urllib.error
 from unittest import mock
 
+import pytest
+import requests
+
 from soliplex import ollama
+
+TEST_OLLAMA_URL = "https://ollama.example.com:11434"
+TEST_MODEL_NAME = "test-model:7.8"
+SOURCE_MODEL_NAME = "dest-model:7.8"
+DEST_MODEL_NAME = "dest-model:7.8"
+MODEL_TEMPLATE = "Test model template"
+MODEL_LICENSE = "Test model license"
+SYSTEM_PROMPT = "Test sytem prompt"
+MODEL_PARAMETERS = {"test-param": 0.1234}
+MODEL_MESSAGES = [{"role": "user", "message": "Why is the sky blue?"}]
+MODEL_QUANTIZE = "q1_K"
+
+
+@pytest.fixture
+def rest_api():
+    return ollama.REST_API(TEST_OLLAMA_URL)
+
+
+@pytest.mark.parametrize(
+    "status_code, expectation",
+    [
+        (None, contextlib.nullcontext()),
+        (400, pytest.raises(requests.exceptions.HTTPError)),
+    ],
+)
+@mock.patch("soliplex.ollama.requests.get")
+def test_ollama_rest_api_get_version(
+    r_get,
+    rest_api,
+    status_code,
+    expectation,
+):
+    if status_code is not None:
+        r_get.side_effect = requests.HTTPError(status_code)
+
+    with expectation as expected:
+        found = rest_api.get_version()
+
+    if status_code is not None:
+        assert expected.value.args == (status_code,)
+    else:
+        assert found is r_get.return_value.json.return_value
+
+    r_get.assert_called_once_with(f"{TEST_OLLAMA_URL}/api/version")
+
+
+@pytest.mark.parametrize(
+    "status_code, expectation",
+    [
+        (None, contextlib.nullcontext()),
+        (400, pytest.raises(requests.exceptions.HTTPError)),
+    ],
+)
+@mock.patch("soliplex.ollama.requests.get")
+def test_ollama_rest_api_get_available_models(
+    r_get,
+    rest_api,
+    status_code,
+    expectation,
+):
+    if status_code is not None:
+        r_get.side_effect = requests.HTTPError(status_code)
+
+    with expectation as expected:
+        found = rest_api.get_available_models()
+
+    if status_code is not None:
+        assert expected.value.args == (status_code,)
+    else:
+        assert found is r_get.return_value.json.return_value
+
+    r_get.assert_called_once_with(f"{TEST_OLLAMA_URL}/api/tags")
+
+
+@pytest.mark.parametrize(
+    "status_code, expectation",
+    [
+        (None, contextlib.nullcontext()),
+        (400, pytest.raises(requests.exceptions.HTTPError)),
+    ],
+)
+@mock.patch("soliplex.ollama.requests.get")
+def test_ollama_rest_api_get_running_models(
+    r_get,
+    rest_api,
+    status_code,
+    expectation,
+):
+    if status_code is not None:
+        r_get.side_effect = requests.HTTPError(status_code)
+
+    with expectation as expected:
+        found = rest_api.get_running_models()
+
+    if status_code is not None:
+        assert expected.value.args == (status_code,)
+    else:
+        assert found is r_get.return_value.json.return_value
+
+    r_get.assert_called_once_with(f"{TEST_OLLAMA_URL}/api/ps")
+
+
+@pytest.mark.parametrize("w_verbose", [None, False, True])
+@pytest.mark.parametrize(
+    "status_code, expectation",
+    [
+        (None, contextlib.nullcontext()),
+        (400, pytest.raises(requests.exceptions.HTTPError)),
+    ],
+)
+@mock.patch("soliplex.ollama.requests.post")
+def test_ollama_rest_api_show_model(
+    r_post, rest_api, status_code, expectation, w_verbose
+):
+    exp_data = {"model": TEST_MODEL_NAME}
+
+    kwargs = {}
+
+    if w_verbose is not None:
+        kwargs["verbose"] = exp_data["verbose"] = w_verbose
+
+    if status_code is not None:
+        r_post.side_effect = requests.HTTPError(status_code)
+
+    with expectation as expected:
+        found = rest_api.show_model(TEST_MODEL_NAME, **kwargs)
+
+    if status_code is not None:
+        assert expected.value.args == (status_code,)
+    else:
+        assert found is r_post.return_value.json.return_value
+
+    r_post.assert_called_once_with(
+        f"{TEST_OLLAMA_URL}/api/show", json=exp_data
+    )
+
+
+@pytest.mark.parametrize("w_stream", [None, False, True])
+@pytest.mark.parametrize(
+    "w_kwargs",
+    [
+        {},
+        {
+            "template": MODEL_TEMPLATE,
+            "license": MODEL_LICENSE,
+            "system_prompt": SYSTEM_PROMPT,
+            "parameters": MODEL_PARAMETERS,
+            "messages": MODEL_MESSAGES,
+            "quantize": MODEL_QUANTIZE,
+        },
+    ],
+)
+@pytest.mark.parametrize(
+    "status_code, expectation",
+    [
+        (None, contextlib.nullcontext()),
+        (400, pytest.raises(requests.exceptions.HTTPError)),
+    ],
+)
+@mock.patch("soliplex.ollama.requests.post")
+def test_ollama_rest_api_create_model(
+    r_post,
+    rest_api,
+    status_code,
+    expectation,
+    w_kwargs,
+    w_stream,
+):
+    exp_data = {
+        "model": TEST_MODEL_NAME,
+        "from": SOURCE_MODEL_NAME,
+    } | w_kwargs
+
+    if "system_prompt" in w_kwargs:
+        exp_data["system"] = exp_data.pop("system_prompt")
+
+    call_kwargs = {}
+
+    if w_stream is not None:
+        exp_data["stream"] = call_kwargs["stream"] = w_stream
+
+    if status_code is not None:
+        r_post.side_effect = requests.HTTPError(status_code)
+
+    with expectation as expected:
+        found = rest_api.create_model(
+            model_name=TEST_MODEL_NAME,
+            source_model_name=SOURCE_MODEL_NAME,
+            **call_kwargs,
+            **w_kwargs,
+        )
+
+    if status_code is not None:
+        assert expected.value.args == (status_code,)
+    else:
+        assert found is r_post.return_value.json.return_value
+
+    r_post.assert_called_once_with(
+        f"{TEST_OLLAMA_URL}/api/create", json=exp_data
+    )
+
+
+@pytest.mark.parametrize(
+    "status_code, expectation",
+    [
+        (None, contextlib.nullcontext()),
+        (400, pytest.raises(requests.exceptions.HTTPError)),
+    ],
+)
+@mock.patch("soliplex.ollama.requests.post")
+def test_ollama_rest_api_copy_model(
+    r_post,
+    rest_api,
+    status_code,
+    expectation,
+):
+    exp_data = {"source": SOURCE_MODEL_NAME, "destination": DEST_MODEL_NAME}
+
+    if status_code is not None:
+        r_post.side_effect = requests.HTTPError(status_code)
+
+    with expectation as expected:
+        found = rest_api.copy_model(
+            SOURCE_MODEL_NAME,
+            DEST_MODEL_NAME,
+        )
+
+    if status_code is not None:
+        assert expected.value.args == (status_code,)
+    else:
+        assert found is r_post.return_value.json.return_value
+
+    r_post.assert_called_once_with(
+        f"{TEST_OLLAMA_URL}/api/copy", json=exp_data
+    )
+
+
+@pytest.mark.parametrize(
+    "status_code, expectation",
+    [
+        (None, contextlib.nullcontext()),
+        (400, pytest.raises(requests.exceptions.HTTPError)),
+    ],
+)
+@mock.patch("soliplex.ollama.requests.post")
+def test_ollama_rest_api_push_model(
+    r_post,
+    rest_api,
+    status_code,
+    expectation,
+):
+    exp_data = {"model": TEST_MODEL_NAME}
+
+    if status_code is not None:
+        r_post.side_effect = requests.HTTPError(status_code)
+
+    with expectation as expected:
+        found = rest_api.push_model(TEST_MODEL_NAME)
+
+    if status_code is not None:
+        assert expected.value.args == (status_code,)
+    else:
+        assert found is r_post.return_value.json.return_value
+
+    r_post.assert_called_once_with(
+        f"{TEST_OLLAMA_URL}/api/push", json=exp_data
+    )
+
+
+@pytest.mark.parametrize("w_stream", [None, False, True])
+@pytest.mark.parametrize(
+    "status_code, expectation",
+    [
+        (None, contextlib.nullcontext()),
+        (400, pytest.raises(requests.exceptions.HTTPError)),
+    ],
+)
+@mock.patch("soliplex.ollama.requests.post")
+def test_ollama_rest_api_pull_model(
+    r_post,
+    rest_api,
+    status_code,
+    expectation,
+    w_stream,
+):
+    exp_data = {"model": TEST_MODEL_NAME}
+    kwargs = {}
+
+    if w_stream is not None:
+        exp_data["stream"] = kwargs["stream"] = w_stream
+
+    if status_code is not None:
+        r_post.side_effect = requests.HTTPError(status_code)
+
+    with expectation as expected:
+        found = rest_api.pull_model(TEST_MODEL_NAME, **kwargs)
+
+    if status_code is not None:
+        assert expected.value.args == (status_code,)
+    else:
+        assert found is r_post.return_value.json.return_value
+
+    r_post.assert_called_once_with(
+        f"{TEST_OLLAMA_URL}/api/pull", json=exp_data
+    )
+
+
+@pytest.mark.parametrize(
+    "status_code, expectation",
+    [
+        (None, contextlib.nullcontext()),
+        (400, pytest.raises(requests.exceptions.HTTPError)),
+    ],
+)
+@mock.patch("soliplex.ollama.requests.delete")
+def test_ollama_rest_api_delete_model(
+    r_delete,
+    rest_api,
+    status_code,
+    expectation,
+):
+    exp_data = {"model": TEST_MODEL_NAME}
+
+    if status_code is not None:
+        r_delete.side_effect = requests.HTTPError(status_code)
+
+    with expectation as expected:
+        found = rest_api.delete_model(TEST_MODEL_NAME)
+
+    if status_code is not None:
+        assert expected.value.args == (status_code,)
+    else:
+        assert found is r_delete.return_value.json.return_value
+
+    r_delete.assert_called_once_with(
+        f"{TEST_OLLAMA_URL}/api/delete",
+        json=exp_data,
+    )
+
 
 # pull_model tests
 
