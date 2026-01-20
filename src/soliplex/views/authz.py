1d1d6b3b7452015fd09097ff80a4a33e6fdd9cdd
Tres Seaver <tseaver@palladion.com>
2026-01-20 13:17:13 -0500

    fix: missing room authz endpoints (v0.34.x)
    - 'POST /api/vi/rooms/{room_id}/authz'
    - 'DELETE /api/vi/rooms/{room_id}/authz'
    
    tests: functest for room authz endpoints
    
    Closes #476.

--- src/soliplex/views/authz.py
+++ src/soliplex/views/authz.py
@@ -41,3 +41,49 @@ async def get_room_authz(
         ) from None
 
     return room_policy
+
+
+@util.logfire_span("POST /v1/rooms/{room_id}/authz")
+@router.post(
+    "/v1/rooms/{room_id}/authz",
+    summary="Update authorization policy for a room",
+)
+async def post_room_authz(
+    request: fastapi.Request,
+    room_id: str,
+    room_policy: models.RoomPolicy,
+    the_installation: installation.Installation = depend_the_installation,
+    the_room_authz: authz_package.RoomAuthorization = depend_the_room_authz,
+    token: security.HTTPAuthorizationCredentials = authn.oauth2_predicate,
+) -> models.RoomPolicy | None:
+    user = authn.authenticate(the_installation, token)
+
+    await the_room_authz.update_room_policy(
+        room_id=room_id,
+        room_policy=room_policy,
+        user_token=user,
+    )
+
+    return fastapi.Response(status_code=204)
+
+
+@util.logfire_span("DELTE /v1/rooms/{room_id}/authz")
+@router.delete(
+    "/v1/rooms/{room_id}/authz",
+    summary="Delete authorization policy for a room",
+)
+async def delete_room_authz(
+    request: fastapi.Request,
+    room_id: str,
+    the_installation: installation.Installation = depend_the_installation,
+    the_room_authz: authz_package.RoomAuthorization = depend_the_room_authz,
+    token: security.HTTPAuthorizationCredentials = authn.oauth2_predicate,
+) -> models.RoomPolicy | None:
+    user = authn.authenticate(the_installation, token)
+
+    await the_room_authz.delete_room_policy(
+        room_id=room_id,
+        user_token=user,
+    )
+
+    return fastapi.Response(status_code=204)
--- tests/unit/views/test_authz_views.py
+++ tests/unit/views/test_authz_views.py
@@ -16,6 +16,17 @@ ROOM_POLICY = models.RoomPolicy(
     default_allow_deny=authz_package.AllowDeny.ALLOW,
 )
 
+NEW_ROOM_POLICY = models.RoomPolicy(
+    room_id=ROOM_ID,
+    default_allow_deny=authz_package.AllowDeny.DENY,
+    acl_entries=[
+        models.ACLEntry(
+            allow_deny=authz_package.AllowDeny.ALLOW,
+            email="phreddy@example.com",
+        ),
+    ],
+)
+
 
 def raises_httpexc(*, match, code) -> pytest.raises:
     def _check(exc):
@@ -39,7 +50,8 @@ def raises_httpexc(*, match, code) -> pytest.raises:
         ),
     ],
 )
-async def test_get_room_authz(w_policy, expectation):
+@mock.patch("soliplex.authn.authenticate")
+async def test_get_room_authz(auth_fn, w_policy, expectation):
     request = fastapi.Request(scope={"type": "http"})
     the_installation = mock.create_autospec(installation.Installation)
     the_room_authz = mock.create_autospec(authz_package.RoomAuthorization)
@@ -62,3 +74,74 @@ async def test_get_room_authz(w_policy, expectation):
 
     if not isinstance(expected, pytest.ExceptionInfo):
         assert found == expected
+
+        the_room_authz.get_room_policy.assert_awaited_once_with(
+            room_id=ROOM_ID,
+            user_token=auth_fn.return_value,
+        )
+
+    auth_fn.assert_called_once_with(the_installation, token)
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("w_existing", [False, True])
+@mock.patch("soliplex.authn.authenticate")
+async def test_post_room_authz(auth_fn, w_existing):
+    request = fastapi.Request(scope={"type": "http"})
+    the_installation = mock.create_autospec(installation.Installation)
+    the_room_authz = mock.create_autospec(authz_package.RoomAuthorization)
+    token = object()
+
+    if w_existing:
+        the_room_authz.get_room_policy.return_value = ROOM_POLICY
+
+    found = await authz_views.post_room_authz(
+        request=request,
+        room_id=ROOM_ID,
+        room_policy=NEW_ROOM_POLICY,
+        the_installation=the_installation,
+        the_room_authz=the_room_authz,
+        token=token,
+    )
+
+    assert isinstance(found, fastapi.Response)
+    assert found.status_code == 204
+
+    the_room_authz.update_room_policy.assert_awaited_once_with(
+        room_id=ROOM_ID,
+        room_policy=NEW_ROOM_POLICY,
+        user_token=auth_fn.return_value,
+    )
+
+    auth_fn.assert_called_once_with(the_installation, token)
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("w_existing", [False, True])
+@mock.patch("soliplex.authn.authenticate")
+async def test_delete_room_authz(auth_fn, w_existing):
+    request = fastapi.Request(scope={"type": "http"})
+    the_installation = mock.create_autospec(installation.Installation)
+    the_room_authz = mock.create_autospec(authz_package.RoomAuthorization)
+    token = object()
+
+    if w_existing:
+        the_room_authz.get_room_policy.return_value = ROOM_POLICY
+
+    found = await authz_views.delete_room_authz(
+        request=request,
+        room_id=ROOM_ID,
+        the_installation=the_installation,
+        the_room_authz=the_room_authz,
+        token=token,
+    )
+
+    assert isinstance(found, fastapi.Response)
+    assert found.status_code == 204
+
+    the_room_authz.delete_room_policy.assert_awaited_once_with(
+        room_id=ROOM_ID,
+        user_token=auth_fn.return_value,
+    )
+
+    auth_fn.assert_called_once_with(the_installation, token)
