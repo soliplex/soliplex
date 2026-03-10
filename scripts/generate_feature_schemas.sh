079462cfcd6ab0e44152e167dfa8e8b058f14a17
Tres Seaver <tseaver@palladion.com>
2026-03-10 17:56:19 -0400

    chore: generate v0.46 AGUI feature schemas

--- scripts/generate_feature_schemas.sh
+++ scripts/generate_feature_schemas.sh
@@ -6,8 +6,8 @@ soliplex-cli agui-feature-schemas "$repo_root/example/installation.yaml" | jq '{
             "$schema": "http://json-schema.org/draft-07/schema#",
             "type": "object",
             "properties": {
-              "filter_documents": .filter_documents.json_schema,
-              "ask_history": .ask_history.json_schema,
-              "haiku.rag.chat": .["haiku.rag.chat"].json_schema
+              "image-generation": .["image-generation"].json_schema,
+              "rag": .rag.json_schema,
+              "rlm": .rlm.json_schema
             }
           }' >"$repo_root/schemas/schema.json"
