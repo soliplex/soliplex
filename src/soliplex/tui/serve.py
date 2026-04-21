d0d02071f42a5a2fc5df40a2f524b7b67df999f7
Tres Seaver <tseaver@palladion.com>
2026-04-20 20:23:04 -0400

    fix: expose '--public-url' option for TUI server

--- src/soliplex/tui/serve.py
+++ src/soliplex/tui/serve.py
@@ -28,18 +28,29 @@ PORT = typer.Option(
     help="Port on which the server runs",
 )
 
+PUBLIC_URL = typer.Option(
+    None,
+    "--public-url",
+    help=(
+        "Publically-reachable URL of the served HTML "
+        "(defaults to 'http://{host}:{port}')"
+    ),
+)
+
 
 @the_cli.command()
 def app(
     soliplex_url: str = BACKEND_URL,
     host: str = HOST,
     port: int = PORT,
+    public_url: str = PUBLIC_URL,
 ):
     """soliplex TUI server"""
     server = server_module.Server(
         f"soliplex-tui --url {soliplex_url}",
         host=host,
         port=port,
+        public_url=public_url,
     )
     server.serve()
 
